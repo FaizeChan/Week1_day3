74ebe5118de25a9c85d63c4d4cd64c76053a6582
FaizeChan <obey.faize@gmail.com>
2017-09-27 14:12:27 -0400

    did switch case example

--- Week1_day3/Program.cs
+++ Week1_day3/Program.cs
@@ -13,20 +13,16 @@ namespace Week1_day3
             int firstNumber = 2;
             int secondNumber = 6;
 
-            if(firstNumber > secondNumber)
+            if (firstNumber > secondNumber)
             {
                 //Do something
                 Console.WriteLine("You have broken Math. Good Job");
             }
 
-
-
-
-
             Console.WriteLine("What day is today?");
             string userAnswer = Console.ReadLine();
 
-            if(userAnswer == "Monday")
+            if (userAnswer == "Monday")
             {
                 Console.WriteLine("Back to work.");
             }
@@ -38,6 +34,72 @@ namespace Week1_day3
             {
                 Console.WriteLine("Bloop");
             }
+
+
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
+
+            if (userAge <= 1)
+            {
+                Console.WriteLine("You're just a baby!");
+
+            }
+            else if (userAge > 1 && userAge < 4)
+            {
+                Console.WriteLine("Yay toddlers!");
+            }
+            else if (userAge >= 4 && userAge < 13)
+            {
+                Console.WriteLine("Enjoy your childhood");
+            }
+            else if (userAge >= 13 && userAge < 65)
+            {
+                Console.WriteLine("Life is awkward.");
+            }
+            else if (userAge >= 65 && userAge < 100)
+            {
+                Console.WriteLine("Enjoy your retirement.");
+            }
+            else
+            {
+                Console.WriteLine("That's not really your age");
+            }
+
+            Console.WriteLine("How many points did you earn?");
+            int pointsEarned = int.Parse(Console.ReadLine());
+
+            switch (pointsEarned)
+            {
+                case 0:
+                    Console.WriteLine("You lose.");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Needs practice.");
+                    break;
+
+                case 2:
+                    Console.WriteLine("On target.");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Superstar.");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Overachiever.");
+                    break;
+
+                case 5:
+                    Console.WriteLine("You cheated.");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid score entered.");
+                    break;
+
+            }
+
         }
-    }
+        }
 }
